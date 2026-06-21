310b3bbdfd7eec8fe7d31d67002302598f142993
Jesse Cordova <jessefcordova98@outlook.com>
2026-06-20 23:33:07 -0300

    atualizada para deploy no github

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Site-para-JBETec/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
